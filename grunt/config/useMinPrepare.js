3eafe872330cadf5e1dfe611e72650da807156e4
Joshua Appelman <jappelman@xebia.com>
2014-05-25 14:35:20 +0200

    Built jbnicolai.com from commit 882f6b7 on branch master

--- grunt/config/useMinPrepare.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  options: {
-    dest: '<%= paths.dist %>'
-  },
-  html: '<%= paths.dist %>/index.html'
-};
--- grunt/config/usemin.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  options: {
-    assetsDirs: '<%= paths.dist %>',
-  },
-  html: ['<%= paths.dist %>/**/*.html'],
-  css: ['<%= paths.dist %>/css/**/*.css']
-};
